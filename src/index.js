c0f75f050304e1e3671bc873f8718179e4296ac7
Matt <mawburn7@gmail.com>
2017-09-06 21:23:31 -0500

    basic scoring

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import hole from './img/hole.png'
 import transition from './img/frame-1.png'
 import mole from './img/frame-2.png'
+import hitMole from './img/hit.png'
+import hitMoleTransition from './img/hit-transition.png'
 
 import normalize from 'normalize.css'
 import css from './styles/style.css'
 
 const spaces = document.querySelectorAll('.space')
+const container = document.getElementById('container')
+const scoreElm = document.getElementById('score')
+let score = 0
+
+scoreElm.textContent = `${score}`
 
 spaces.forEach((img) => img.src = hole)
 
@@ -18,18 +25,31 @@ setInterval(() => {
 
   setTimeout(() => {
     space.src = mole
+    space.dataset.scorable = true
 
     setTimeout(() => {
-      space.src = transition
+      space.src = (space.dataset.scorable === 'false') ? hitMoleTransition : transition
+      space.dataset.scorable = false
 
       setTimeout(() => {
         space.src = hole
       }, 100) 
-    }, 1000)
+    }, (1000 - score*20)) 
   }, 100)
-}, 250)
+}, (1000 - score*50)) 
 
-function getRandomSpace(hole) {
+container.addEventListener('click', (e) => {
+  const target = e.target
+
+  if(target && target.nodeName === 'IMG' && target.dataset.scorable === 'true') {
+    ++score
+    scoreElm.textContent = `${score}`
+    target.dataset.scorable = false
+    target.src = hitMole
+  }
+})
+
+function getRandomSpace(hole, delay = false) {
   const spaceNum = Math.floor(Math.random() * (9) + 1)
   const elm = document.getElementById(`space-${spaceNum}`)
 
@@ -39,5 +59,11 @@ function getRandomSpace(hole) {
     return elm
   }
 
-  return getRandomSpace(hole)
+  if(delay) {
+    return setTimeout(() => {
+      return getRandomSpace(hole, false)
+    }, 250)
+  }
+
+  return getRandomSpace(hole, true)
 }
